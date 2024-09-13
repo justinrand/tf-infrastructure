d6c361f261aea9245431fb199182cf260849d0ee
jrand <justin.rand@icloud.com>
2024-09-13 12:13:12 -0400

    refactor: :art: convert user_data to template

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "local_file" "default_key" {
   file_permission = "0600"
 }
 
-resource "aws_instance" "zookeeper" {
+resource "aws_instance" "zk" {
   count = var.zookeeper_count
 
   ami               = data.aws_ssm_parameter.ami.value
@@ -121,43 +121,16 @@ resource "aws_instance" "zookeeper" {
   ]
   key_name = aws_key_pair.default.key_name
   subnet_id = data.aws_subnet.gateway.id
-  user_data = <<EOF
-#!/bin/bash -x
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-yum install docker -y
-usermod -a -G docker ec2-user
-systemctl enable docker
-systemctl start docker
-
-cat <<EOT >> /etc/hosts
-${aws_network_interface.zk[0].private_ip} zk1.local
-${aws_network_interface.zk[1].private_ip} zk2.local
-${aws_network_interface.zk[2].private_ip} zk3.local
-EOT
-
-iptables -I INPUT -p tcp --dport 2181 -j ACCEPT
-iptables -I INPUT -p tcp --dport 2182 -j ACCEPT
-iptables -I INPUT -p tcp --dport 2888 -j ACCEPT
-iptables -I INPUT -p tcp --dport 3888 -j ACCEPT
-
-mkdir -p /var/log/zookeeper
-mkdir -p /etc/zookeeper
-mkdir -p /data/zookeeper
-
-docker run -d \
---net=host \
---name=zookeeper \
--e ZOOKEEPER_CLIENT_PORT=${var.zookeeper_client_port} \
--e ZOOKEEPER_SERVER_ID=${count.index + 1} \
--e ZOOKEEPER_SERVERS=zk1.local:2888:3888\;zk2.local:2888:3888\;zk3.local:2888:3888 \
--p 2888:2888 \
--p 3888:3888 \
--p 2181:2181 \
--p 2182:2182 \
--v /data/zookeeper/:/data \
--v /var/log/zookeeper:/logs \
-confluentinc/cp-zookeeper:${var.zookeeper_version}
-  EOF
+  user_data = templatefile("user_data.tftpl", {
+    zk_servers = {
+      "zk1.local" = "${aws_network_interface.zk[0].private_ip}", 
+      "zk2.local" = "${aws_network_interface.zk[1].private_ip}", 
+      "zk3.local" = "${aws_network_interface.zk[2].private_ip}"
+    }, 
+    client_port = 2182,
+    zk_id = count.index + 1,
+    zk_version = var.zookeeper_version
+  })
 
   tags = {
     Name = "zookeeper"
@@ -175,10 +148,24 @@ resource "aws_network_interface_attachment" "zk" {
   count = var.zookeeper_count
 
   device_index = 1
-  instance_id = aws_instance.zookeeper[count.index].id
+  instance_id = aws_instance.zk[count.index].id
   network_interface_id = aws_network_interface.zk[count.index].id
 }
 
 output "zk_ip" {
-  value = aws_instance.zookeeper.*.public_ip
+  value = aws_instance.zk.*.public_ip
+}
+
+resource "local_file" "zk_config" {
+  filename = "zk.sh"
+  content = templatefile("user_data.tftpl", {
+    zk_servers = {
+      "zk1.local" = "${aws_network_interface.zk[0].private_ip}", 
+      "zk2.local" = "${aws_network_interface.zk[1].private_ip}", 
+      "zk3.local" = "${aws_network_interface.zk[2].private_ip}"
+    }, 
+    client_port = 2182,
+    zk_id = 1,
+    zk_version = var.zookeeper_version
+  })
 }
